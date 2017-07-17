0bda24d7ab98da92066f66f29ab3abc2a0b2bf42
Anh Pham <anhpham.march@gmail.com>
2017-07-17 23:06:45 +0700

    05_01

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" type="text/css" href="formatstyles.css"/>
     <link rel="stylesheet" type="text/css" href="layoutstyle.css"/>
     <title>Master template </title>
-    <meta name="description" content="???????" />
-    <meta name="keywords" content="???,???,???" />
+    <meta name="description" content="Home Page e_bus class" />
+    <meta name="keywords" content="ebus, html, css, php, etc" />
 
     </template>
   </head>
@@ -15,12 +15,12 @@
     <!-- Start of the page section -->
     <div id="wrapper">
       <div>
-        <h2 id="header">Banner</h2>
+        <h2 id="header">Home Page</h2>
       </div>
 
 <!-- Start of the page section -->
       <div>
-        <!-- <h2 id"leftmenu">Left Nagivation</h2>-->
+      <!--<h2 id="leftmenu">Left Nagivation</h2> -->
         <?php include ("menu.php"); ?>
         Option 2 <br />
         Option 3 <br />
@@ -29,7 +29,8 @@
 
 <!-- Start of the page section -->
       <div>
-        <h2 id="content">Content</h2>
+        <h2 id="content">Main content</h2>
+        <img src="http://icons.iconarchive.com/icons/raindropmemory/harmonia-pastelis/256/hp-girl-icon.png" alt="Girl">
       </div>
 
 <!-- Start of the page section -->
