d42e4fa1958751ad7c5d4a03c936a9e40d030312
Anh Pham <anhpham.march@gmail.com>
2017-07-25 08:33:37 +0700

    style_user

---
+++ register_userinfo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Check Info page</title>
+  </head>
+  <body>
+    <?php
+     $firstnamename ="";
+     $lastname ="";
+     $gender ="";
+     $address ="";
+     $city ="";
+     $phonenumber ="";
+     $email ="";
+     $password ="";
+
+     $firstname =$_POST["firstname"];
+     $lastname =$_POST["lastname"];
+     $gender =$_POST["gender"];
+     $address =$_POST["address"];
+     $city =$_POST["city"];
+     $phonenumber =$_POST["phonenumber"];
+     $email =$_POST[""];
+     $password =$_POST["password"];
+
+     echo "Sign up successfully";
+     ?>
+  </body>
+</html>
